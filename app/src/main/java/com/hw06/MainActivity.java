d3c1047f357479271f9e3a07f29abed35b23e1bc
fangdai <pq.dai225@gmail.com>
2021-03-24 22:55:28 +0700

    Update

--- app/src/main/java/com/hw06/MainActivity.java
+++ app/src/main/java/com/hw06/MainActivity.java
@@ -13,6 +13,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,12 +28,13 @@ public class MainActivity extends AppCompatActivity {
     long startingMills = System.currentTimeMillis();
     final int MAX_PROGRESS = 100;
     Handler myHandler = new Handler();
-
+    Random ran = new Random();
+    boolean isRunning = false;
+    ArrayList<View> viewsToFadeIn = new ArrayList<View>();
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         txtBox = findViewById(R.id.txtBox);
         progressBar = findViewById(R.id.progressBar);
         txtPercent = findViewById(R.id.txtPercent);
@@ -40,8 +45,11 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(View v) {
                         btnDoItAgain.setEnabled(false);
                         inputVal = Integer.parseInt(txtBox.getText().toString());
-                        //isRunning = true;
-                        onStart();
+                        isRunning = true;
+                        //Toast.makeText(MainActivity.this, ""+isRunning.getText().toString(),1).show();
+                        if(isRunning == true){
+                            onStart();}
+
                     }
 
                 }
@@ -51,7 +59,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-
+        viewsToFadeIn.add(findViewById(R.id.btnDoItAgain));
+        for (View vz : viewsToFadeIn)
+        {
+            vz.setAlpha(0); // make invisible to start
+        }
         txtBox.setHint("Input a number");
         txtBox.setText("");
         progress=0;
@@ -69,12 +81,24 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             try{
                 progressBar.incrementProgressBy(inputVal);
-                progress += inputVal;
+                //progress += (inputVal <= 0) ? 1 : ran.nextInt(inputVal - 1); // Tang dan
+                progress += 1; // basic
+
                 txtPercent.setText(progress+"%");
+                for (View v : viewsToFadeIn)
+                {
+                    // 3 second fade in time
+                    v.animate().alpha(progress%100).start();
+                }
                 if (progress >= progressBar.getMax()) {
                     progressBar.setProgress(MAX_PROGRESS);
                     btnDoItAgain.setEnabled(true);
                     txtPercent.setText(MAX_PROGRESS+"%");
+                    for (View v : viewsToFadeIn)
+                    {
+                        // 3 second fade in time
+                        v.setAlpha(1);
+                    }
                 }
             }
             catch (Exception e){Log.e("<<foregroundTask>>",e.getMessage());}
